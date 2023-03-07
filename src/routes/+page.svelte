d1efb63f18f23b1976cec06d24a1ad0772f0151f
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-03-07 19:04:54 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
 	export let title = 'Mes playlist';
 	let count = 1;
-	let nb1 = 10; 
-	let nb2 = 5; 
-
+	let nb1 = 10;
+	let nb2 = 5;
+    let result = 0;
 	function handleClick() {
 		count = count + 1;
 	}
+
+	function calculate() {
+		 result = nb1 * nb2;
+	}
+	calculate();
 </script>
 
 <h1>{title}</h1>
@@ -15,4 +20,11 @@
 
 <div>{count}</div>
 
-<input bind:value={nb1} /> * <input bind:value={nb2} />
+<input bind:value={nb1} on:keyup={calculate} /> * <input bind:value={nb2} on:keyup={calculate} /> {result}
+
+
+{#if count %2 === 0 }
+	<p>Ce nombre est divisible par deux</p>
+{:else}
+	<p>Ce nombre n'est divisible par deux</p>
+{/if}
