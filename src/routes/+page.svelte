e34d9699d5ce4882ab8e4785b5ba10456b9503cc
Samuel Lassoie <samuel.lassoie@gmail.com>
2023-03-07 18:42:49 +0100

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let title = 'Mes playlist';
+	let count = 1;
+	let nb1 = 10; 
+	let nb2 = 5; 
+
+	function handleClick() {
+		count = count + 1;
+	}
+</script>
+
+<h1>{title}</h1>
+
+<button on:click={handleClick}>Incrémenter</button>
+
+<div>{count}</div>
+
+<input bind:value={nb1} /> * <input bind:value={nb2} />
